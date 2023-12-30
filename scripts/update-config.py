177cd22a0b8ad923d28f4d1fe8e252fbda4d535f
Daxter (ZiggyC) <119143393+DaxterOtter@users.noreply.github.com>
2023-12-30 11:41:00 -0600

    Update update-config.py
    Adds in the '--us-feeder' arg

--- scripts/update-config.py
+++ scripts/update-config.py
@@ -3,15 +3,14 @@
 import argparse
 import requests
 
-
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--tag", type=str, default="master")
     parser.add_argument("--custom", type=str, required=True)
     parser.add_argument("--config", type=str, default="global")
+    parser.add_argument("--us-feeder", action="store_true")  # Add this line for --us-feeder
 
     return parser.parse_args()
-
 
 def main():
     args = parse_args()
@@ -23,9 +22,12 @@ def main():
         return
 
     config = open(args.custom, "r").read() + "\n"
+    combined_config = config + response.text
 
-    print(config + response.text)
+    if args.us_feeder:  # Check if --us-feeder is set
+        combined_config = combined_config.replace('"binance"', '"binanceus"')
 
+    print(combined_config)
 
 if __name__ == "__main__":
     main()
